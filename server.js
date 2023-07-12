b3f690c719f9cf4ab6b2199be7eda92bc03a7a17
BilelMH <bilel.mh1997@gmail.com>
2023-07-12 19:33:46 +0100

    express

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require ('express');
+const fs = require('fs');
+const app= express();
+const PORT =4500;
+const v = require('./Verif');
+const { log } = require('console');
+
+app.get('/' ,(req,res)=>{
+    fs.readFile('./NavbarHome.html',v,'utf8',(err,data)=>{
+        err ? res.send('file not found')&& console.error(err) : res.send(data)
+    })
+})
+app.get('/NavbarHome.html',v, (req,res)=>{
+    fs.readFile('./NavbarHome.html','utf8',(err,data)=>{
+        err ? res.send('file not found')&& console.error(err) : res.send(data)
+    })
+})
+app.get('/OurServices.html',v, (req,res)=>{
+    fs.readFile('./OurServices.html','utf8',(err, data)=>{
+        err ? res.send('file not found')&& console.error(err) : res.send(data)
+    })
+})
+app.get('/Contactus.html',v, (req,res)=>{
+    fs.readFile('./Contactus.html','utf8',(err,data)=>{
+        err ? res.send('file not found')&& console.error(err) : res.send(data)
+    })
+})
+
+
+
+
+
+
+
+
+
+app.listen(PORT,(req,res)=>{
+    console.log(`server running : port: ${PORT}`)
+})
